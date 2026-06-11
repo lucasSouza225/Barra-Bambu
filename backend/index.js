7a721d52a33320f47ea62c25479ec0f7b71455f9
LUCAS DE SOUZA SANTOS <lucas2469y@gmail.com>
2026-06-10 22:12:42 -0300

    ajustes na api

--- backend/index.js
+++ backend/index.js
@@ -18,14 +18,18 @@ const __dirname = path.dirname(__filename);
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(cors({
+/* app.use(cors({
     origin: 'http://localhost:5173',
     credentials: true
-}));
-/* app.use(cors({
-    origin: 'https://frontend-jg7gh2uha-lucas-projects225.vercel.app',
-    credentials: true
 })); */
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://frontend-gules-two-70.vercel.app',
+    'https://frontend-axfbp6hai-lucas-projects225.vercel.app'
+  ],
+  credentials: true
+}));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.use("/users", UserRouter)
