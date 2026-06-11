61c17d9d615673883501a5d5fcc9cba70413415c
LUCAS DE SOUZA SANTOS <lucas2469y@gmail.com>
2026-06-10 22:25:58 -0300

    voltando ao normal

--- backend/index.js
+++ backend/index.js
@@ -18,16 +18,14 @@ const __dirname = path.dirname(__filename);
 app.use(express.json());
 app.use(cookieParser());
 
-/* app.use(cors({
+app.use(cors({
     origin: 'http://localhost:5173',
     credentials: true
+}));
+/* app.use(cors({
+    origin: 'https://frontend-jg7gh2uha-lucas-projects225.vercel.app',
+    credentials: true
 })); */
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://frontend-naia22urs-lucas-projects225.vercel.app');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.use("/users", UserRouter)
